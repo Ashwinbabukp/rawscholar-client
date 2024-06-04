50f810429ffa0054846d539dd8ab18ff06337785
Ashwinbabukp <kpashwin045@gmail.com>
2024-06-05 02:49:32 +0530

    login and signup page created

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -9,13 +9,22 @@ function Login({ show }) {
   show(false);
   return (
     <div className="containerlogin d-flex justify-content-center align-items-center">
-      <img className='p-1  d-none d-sm-block' src={StudyIllustration} alt="" height="700"  />
+      <img
+        className="p-1  d-none d-sm-block"
+        src={StudyIllustration}
+        alt=""
+        height="700"
+      />
       <form className="formContainer shadow d-flex flex-column align-items-center gap-3">
-        <img className='mb-4' src={Logo} alt="" width={200} />
+        <img className="mb-4" src={Logo} alt="" width={200} />
         <div className="form-group mb-2">
           <p className="m-0">Email</p>
-          <input className="inputBox" type="text" placeholder="Enter Email" 
-          style={{ fontSize: '15px' }} />
+          <input
+            className="inputBox"
+            type="text"
+            placeholder="Enter Email"
+            style={{ fontSize: '15px' }}
+          />
         </div>
         <div className="form-group mb-2">
           <p className="m-0">Password</p>
@@ -26,13 +35,21 @@ function Login({ show }) {
             style={{ fontSize: '15px' }}
           />
         </div>
-        <Button  className='mt-2 mb-3 w-100 ' style={{fontSize: '13px'}} type="submit">Login</Button>
-          <div>
-            <a style={{fontSize:"13px"}} href="/">Forgotten Password?</a>
-          </div>
+        <Button
+          className="mt-2 mb-3 w-100 "
+          style={{ fontSize: '13px' }}
+          type="submit"
+        >
+          Login
+        </Button>
+        <div>
+          <a style={{ fontSize: '13px' }} href="/">
+            Forgotten Password?
+          </a>
+        </div>
         <p className=" mt-5" style={{ fontSize: '13px' }}>
           Don’t have account yet?
-          <Link  to={'/signup'} style={{ color: 'blue', fontSize: '13px' }}>
+          <Link to={'/signup'} style={{ color: 'blue', fontSize: '13px' }}>
             Sign Up
           </Link>
         </p>
